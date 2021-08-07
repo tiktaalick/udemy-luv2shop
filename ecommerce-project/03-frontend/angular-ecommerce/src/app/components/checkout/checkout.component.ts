afa76658464f75a8a337221ab5963f4ec52e2970
tiktaalick <blogfeedser@gmail.com>
2021-08-07 20:32:50 +0200

    Billing Address validations added.

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -69,11 +69,22 @@ export class CheckoutComponent implements OnInit {
           FormValidators.notOnlyWhitespace])
       }),
       billingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: ['']
+        street: new FormControl('',
+          [Validators.required,
+          Validators.minLength(2),
+          FormValidators.notOnlyWhitespace]),
+        city: new FormControl('',
+          [Validators.required,
+          Validators.minLength(2),
+          FormValidators.notOnlyWhitespace]),
+        state: new FormControl('',
+          [Validators.required]),
+        country: new FormControl('',
+          [Validators.required,]),
+        zipCode: new FormControl('',
+          [Validators.required,
+          Validators.minLength(2),
+          FormValidators.notOnlyWhitespace])
       }),
       creditCard: this.formBuilder.group({
         cardType: [''],
@@ -112,6 +123,12 @@ export class CheckoutComponent implements OnInit {
   get shippingAddressState() { return this.checkoutFormGroup.get('shippingAddress.state'); }
   get shippingAddressCountry() { return this.checkoutFormGroup.get('shippingAddress.country'); }
   get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }
+
+  get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street'); }
+  get billingAddressCity() { return this.checkoutFormGroup.get('billingAddress.city'); }
+  get billingAddressState() { return this.checkoutFormGroup.get('billingAddress.state'); }
+  get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country'); }
+  get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode'); }
 
   copyShippingAddressToBillingAddress(event: any) {
     if (event.target.checked) {
